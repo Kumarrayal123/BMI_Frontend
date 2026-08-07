8e9a98f4849e4ca7f1300922a46768d81f7f3044
SAQUIBAWASI <saquibawasi@gmail.com>
2026-08-07 11:22:12 +0530

    Configure API_BASE_URL to localhost for local dev

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
     // API_BASE_URL: "https://bmi-backend-1-nnpo.onrender.com/api", // Production
-    API_BASE_URL: "http://62.72.29.27:5000/api", // Local Development
+    API_BASE_URL: "http://localhost:5000/api", // Local Development (Changed from 62.72.29.27)
 };
 
 export default config;
